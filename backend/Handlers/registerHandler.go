d5893b9e57929bbc57492682a04053ee739027c9
H A S S A N <128993569+7assanyahya@users.noreply.github.com>
2024-10-15 22:08:39 +0300

    spop

--- backend/Handlers/registerHandler.go
+++ backend/Handlers/registerHandler.go
@@ -7,21 +7,23 @@ import (
 	"real-time-forum/backend/structs"
 )
 
-func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		r.ParseForm()
+func RegisterHandler() {
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			r.ParseForm()
 
-		db, err := sql.Open("sqlite3", "./real-forum.db")
-		if err != nil {
-			http.Error(w, "Database connection error", http.StatusInternalServerError)
-			return
-		}
-		defer db.Close()
+			db, err := sql.Open("sqlite3", "./real-forum.db")
+			if err != nil {
+				http.Error(w, "Database connection error", http.StatusInternalServerError)
+				return
+			}
+			defer db.Close()
 
-		err = backend.InsertUser(db, structs.User{})
-		if err != nil {
-			http.Error(w, "Failed to register user", http.StatusInternalServerError)
-			return
+			err = backend.InsertUser(db, structs.User{})
+			if err != nil {
+				http.Error(w, "Failed to register user", http.StatusInternalServerError)
+				return
+			}
 		}
-	}
+	})
 }
